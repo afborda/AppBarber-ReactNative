af3c580223a9600264369acc920b0ffd5276b1e6
Abner Fonseca <abner.borda@gmail.com>
2020-11-28 02:54:36 -0300

    Adicionando preload

---
+++ src/stack/MainStack.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import Preload from '../page/Preload';
+import SignIn from '../page/SignIn';
+import SignUp from '../page/SignUp';
+
+const Stack = createStackNavigator();
+
+export default () => (
+  <Stack.Navigator
+    initialRouterName="Preload"
+    screenOptions={{headerShown: false}}>
+    <Stack.Screen name="Preload" component={Preload} />
+    <Stack.Screen name="SignIn" component={SignIn} />
+    <Stack.Screen name="SignUp" component={SignUp} />
+  </Stack.Navigator>
+);
